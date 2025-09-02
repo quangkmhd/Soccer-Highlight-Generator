5faa41d728a4a5d83a097c057b74ab072af84ca3
quangkmhd <quangnhvn34@gmail.com>
2025-09-02 08:08:23 +0000

    change folder

--- api/__init__.py
+++
@@ -1,5 +0,0 @@
-"""API package for Soccer Action Spotting.
-
-This file makes the `api` directory a Python package so that
-`uvicorn` can import `api.main_api:app` when running with workers.
-"""
--- api/ui/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Gradio demo package for Soccer Action Spotting.
-Exposes client and UI constructors for reuse.
-"""
-from .client import SoccerAPIClient
-from .ui import create_demo
-
-__all__ = ["SoccerAPIClient", "create_demo"]
